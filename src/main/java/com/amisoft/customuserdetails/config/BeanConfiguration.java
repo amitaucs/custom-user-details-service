d1adc1ae06497452212a14bf9fdad001e985a8b4
theamistudy <the.amistudy@gmail.com>
2019-02-16 23:38:43 +0530

    Password encoder added.

--- src/main/java/com/amisoft/customuserdetails/config/BeanConfiguration.java
+++ src/main/java/com/amisoft/customuserdetails/config/BeanConfiguration.java
@@ -3,28 +3,36 @@ package com.amisoft.customuserdetails.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 public class BeanConfiguration {
 
 
     @Bean
-    PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    PasswordEncoder oldPasswordEncoder(){
+
+        String md5 = "MD5";
+
+        return new DelegatingPasswordEncoder(md5,
+                Collections.singletonMap(md5,new MessageDigestPasswordEncoder(md5)));
     }
+
+
 
     @Bean
     CustomUserDetailsService customUserDetailsService(){
 
         Collection<UserDetails> users = Arrays.asList(
 
-                new CustomUserDetails("amisoft", passwordEncoder().encode("password4"),true,"USER"),
-                new CustomUserDetails("amit", passwordEncoder().encode("password3"),true,"ADMIN")
+                new CustomUserDetails("amisoft", oldPasswordEncoder().encode("password5"),true,"USER"),
+                new CustomUserDetails("amit", oldPasswordEncoder().encode("password3"),true,"ADMIN")
         );
 
         return new CustomUserDetailsService(users);
